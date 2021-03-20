07c88367f1f520e924c0ee96566b45ff00a8f6f2
Wilson <wilsonst.suarez@hotmail.com>
2021-03-20 17:25:30 -0500

    Add product interaction

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,46 @@ setInterval(function () {
         counter = 1;
     }
 }, 5000);
+
+
+const lutsbtn = document.querySelectorAll('.lutbtn1');
+const lutimgs = document.querySelector('.lutimg');
+
+function selectlut(elem,i){
+    function lutinput () {
+            
+        switch (i) {
+            case 0:
+                lutimgs.classList.remove('lutimg--2')
+                lutimgs.classList.remove('lutimg--3')
+                lutimgs.classList.remove('lutimg--4')
+                lutimgs.classList.add('lutimg--1');
+                break;
+            case 1:
+                lutimgs.classList.remove('lutimg--1')
+                lutimgs.classList.remove('lutimg--4')
+                lutimgs.classList.remove('lutimg--3')
+                lutimgs.classList.add('lutimg--2')
+                break;
+        
+            case 2:
+                lutimgs.classList.remove('lutimg--1')
+                lutimgs.classList.remove('lutimg--2')
+                lutimgs.classList.remove('lutimg--4')
+                lutimgs.classList.add('lutimg--3')
+                break;
+
+            case 3:
+                lutimgs.classList.remove('lutimg--1')
+                lutimgs.classList.remove('lutimg--2')
+                lutimgs.classList.remove('lutimg--3')
+                lutimgs.classList.add('lutimg--4')
+                break;
+            
+    }
+
+    }
+    elem.addEventListener('click',lutinput);
+}
+    
+lutsbtn.forEach(selectlut);
